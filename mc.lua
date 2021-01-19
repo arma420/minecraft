b8c1f292a6e50c824bcbeb76c7fce733374a1696
MegaArma <61148002+MegaArma@users.noreply.github.com>
2021-01-19 22:18:43 +0200

    Update mc.lua

--- mc.lua
+++ mc.lua
@@ -1,5 +1,10 @@
 local monitor = peripheral.find("monitor")
 local speaker = peripheral.find("speaker")
+local modem = rednet.open("back")
+
+
+
+
 monitor.clear()
 speaker.playSound("minecraft:block.note_block.harp")
 local amount = 10
@@ -15,7 +20,7 @@ monitor.setCursorPos(15,7)
 monitor.blit("PRADETI","1111111","2222222")
 while inMain do
 local event, button, x, y = os.pullEvent("monitor_touch")
-if  x == 7 and y == 15 then
+if  x >= 7 and x >= 14 and y == 15 then
 inMain = false
 Game()
 end
@@ -33,11 +38,13 @@ monitor.setCursorPos(17,8)
 monitor.blit("+","1","2")
 monitor.setCursorPos(23,8)
 monitor.blit("-","1","2")
-monitor.setCursorPos(15,10)
+monitor.setCursorPos(18,10)
 monitor.blit("ZAISTI","111111","222222")
+monitor.setCursorPos(18,13)
+monitor.blit("Baigti","111111","222222")
 while inMainGame do
 local event, button, x, y = os.pullEvent("monitor_touch")
-if   x == 17 and y == 8 then
+if  x == 17 and x <= 19 and y == 8 then
 amount = amout + 1
 monitor.setCursorPos(15,7)
 monitor.clearLine()
@@ -45,7 +52,7 @@ monitor.write("Statymas:" ..amount.." Eur.")
 monitor.setCursorPos(15,12)
 monitor.write("Balansas:" ..balance - amount.." Eur.")
 end
-if  x == 23 and y == 8 then
+if  x >= 23 and x <= 24  and y == 8 then
 amount = amount - 1
 if amount < 1 then
 amount = 1
@@ -56,47 +63,58 @@ monitor.write("Statymas:" ..amount.." Eur.")
 monitor.setCursorPos(15,12)
 monitor.write("Balansas:" ..balance-amount.." Eur.")
 end
-if x == 15 and y == 10 then
+if x >= 18 and x <= 25 and y == 10 then
 inMainGame = false
 rollDice()
 end
+if x >= 18 and x <= 25 and y == 13 then
+inMainGame = false
+End()
+end
 end
 end 
+function End()
+Main()
 
+end
 function rollDice()
 monitor.clear()
 local player = 0
 local ai = 0
+monitor.setCursorPos(16,4)
+monitor.write("Tavo:")
+monitor.setCursorPos(26,4)
+monitor.write("Host:")
 monitor.setCursorPos(1,1)
 monitor.write(math.floor(player))
-monitor.setCursorPos(6,6)
+monitor.setCursorPos(20,6)
 math.randomseed(os.time()-4444)
 local dice = math.floor(math.random(1,6))
 player = player + dice
 monitor.write(dice)
-monitor.setCursorPos(20,6)
+monitor.setCursorPos(30,6)
 math.randomseed(os.time()+410)
 local dice = math.floor(math.random(1,6))
 ai = ai + dice
 monitor.write(dice)
-monitor.setCursorPos(6,7)
+monitor.setCursorPos(20,7)
 math.randomseed(os.time()-150)
 local dice = math.floor(math.random(1,6))
 player = player + dice
 monitor.write(dice)
-monitor.setCursorPos(20,7)
+monitor.setCursorPos(30,7)
 math.randomseed(os.time()+12)
 local dice = math.floor(math.random(1,6))
 ai = ai + dice
-monitor.write(dice)
-monitor.setCursorPos(1,1)
-monitor.write(math.floor(player))
-monitor.setCursorPos(1,2)
+monitor.setCursorPos(30,8)
 monitor.write(ai)
+monitor.setCursorPos(20,8)
+monitor.write(player)
+
 if player > ai then
 monitor.clear()
 monitor.setCursorPos(15,10)
-monitor.write("Laimejai " ..amount*2.." Eur.")
+monitor.write("Laimejai " .. amount * 2 .." Eur.")
 balance = balance + amount
 elseif player < ai then
 monitor.clear()
@@ -115,7 +133,7 @@ monitor.setCursorPos(15,12)
 monitor.blit("Testi","11111","aaaaa")
 while true do
 local event, button, x, y = os.pullEvent("monitor_touch")
-if   x == 15 and y == 12 then
+if   x >= 15 and x <= 20 and y == 12 then
 Game()
 end
 end
