bf12902a7d05574f44ddd47f2bfe3c368b784327
MegaArma <61148002+MegaArma@users.noreply.github.com>
2021-01-19 21:13:57 +0200

    Update mc.lua

--- mc.lua
+++ mc.lua
@@ -15,9 +15,11 @@ while inMain do
 local event, button, x, y = os.pullEvent("monitor_touch")
 if  x == 7 and y == 15 then
 inMain = false
+Game()
 end
 end
 
+function Game()
 monitor.clear()
 local inMainGame = true
 
@@ -54,30 +56,34 @@ monitor.write("Balansas:" ..balance-amount.." Eur.")
 end
 if x == 15 and y == 10 then
 inMainGame = false
+rollDice()
 end
 end
+end 
+
+function rollDice()
 monitor.clear()
 local player = 0
 local ai = 0
 monitor.setCursorPos(1,1)
 monitor.write(math.floor(player))
 monitor.setCursorPos(6,6)
-math.randomseed(os.time())
+math.randomseed(os.time()-4444)
 local dice = math.floor(math.random(1,6))
 player = player + dice
 monitor.write(dice)
 monitor.setCursorPos(20,6)
-math.randomseed(os.time())
+math.randomseed(os.time()+410)
 local dice = math.floor(math.random(1,6))
 ai = ai + dice
 monitor.write(dice)
 monitor.setCursorPos(6,7)
-math.randomseed(os.time())
+math.randomseed(os.time()-150)
 local dice = math.floor(math.random(1,6))
 player = player + dice
 monitor.write(dice)
 monitor.setCursorPos(20,7)
-math.randomseed(os.time())
+math.randomseed(os.time()+12)
 local dice = math.floor(math.random(1,6))
 ai = ai + dice
 monitor.write(dice)
@@ -85,12 +91,30 @@ monitor.setCursorPos(1,1)
 monitor.write(math.floor(player))
 monitor.setCursorPos(1,2)
 monitor.write(ai)
+if player > ai then
+monitor.clear()
+monitor.setCursorPos(15,10)
+monitor.write("Laimejai" ..amount *2)
+balance = balance + amount * 2
+elseif player < ai then
+monitor.clear()
+monitor.setCursorPos(15,10)
+monitor.write("Pralaimejai")
+balance = balance + amount * 2
+balance = balance - amount
+elseif player == ai then
+monitor.clear()
+monitor.setCursorPos(15,10)
+monitor.write("Lygios")
+balance = balance + amount * 2
+balance = balance
+end
+monitor.setCursorPos(15,12)
+monitor.write("Testi")
+while true do
+local event, button, x, y = os.pullEvent("monitor_touch")
+if   x == 15 and y == 12 then
+Game()
+end
+end
 
-function getDice()
-local dice
-for i=1,4 do
-math.randomseed(os.time)
-dice[i] = math.random(1,6)
-end
-return dice
-end
